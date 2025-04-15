399dba2e3c493221f18d91844fa47806d3014c3d
Murat Kasimov <iokasimov.m@gmail.com>
2025-04-15 20:38:49 +0700

    Fix printing out positions, elaborate next steps

--- Ya/Operators/Generate/Main.hs
+++ Ya/Operators/Generate/Main.hs
@@ -5,7 +5,10 @@ import Ya.ASCII
 import Ya.World
 import Ya.Console
 
-parentheses x = output `ha` Glyph `ha` Symbol `ha` Bracket `ha` Opened `hv` Round
+data Variable = Parametric | Positioned
+
+parentheses x = output `ha` Glyph `ha` Symbol `ha` Punctuate `ha` Space `hv` Unit
+ `lu'yp` output `ha` Glyph `ha` Symbol `ha` Bracket `ha` Opened `hv` Round
  `lu'yp` x `lu'yp` output `ha` Glyph `ha` Symbol `ha` Bracket `ha` Closed `hv` Round
 
 print_subtree subtree = Empty @List `hu` enter @World
@@ -13,13 +16,14 @@ print_subtree subtree = Empty @List `hu` enter @World
  `li` unwrap subtree
 
 print_tree = top `ho` this `ho` output
- `lo__'yp` sub `ho` this @(List `WR` Tree ASCII) `ho` print_subtree
+ `lo__'yp` sub `ho` this `ho` print_subtree
 
 print = this `he'ho'he` (shaft `hv` by Passed) `ho` this `ho_'yokl` Forth `ha` World `ha` print_tree
  `lo__'yp` Some `hu_` output `ha` Glyph `ha` Symbol `ha` Bracket `ha` Opened `hv` Round
  `lo__'yp` this `he'ho'he` focus `ho` this `ho_'yokl` Forth `ha` World `ha` print_tree
  `lo__'yp` Some `hu_` output `ha` Glyph `ha` Symbol `ha` Bracket `ha` Closed `hv` Round
- `lo__'yp` this `he'ho'he` (shaft `hv` by Passed) `ho` this `ho_'yokl` Forth `ha` World `ha` print_tree
+ `lo__'yp` this `he'ho'he` (shaft `hv` by Future) `ho` this `ho_'yokl` Forth `ha` World `ha` print_tree
+ `lo__'yp` Some `hu_` output `ha` Glyph `ha` Symbol `ha` Punctuate `ha` Comma `hv` Unit
  `lo__'yp` Some `hu_` output `ha` Glyph `ha` Symbol `ha` Punctuate `ha` Space `hv` Unit
 
 arguments = Nonempty @List
@@ -27,6 +31,12 @@ arguments = Nonempty @List
  `ha__` Next `ho_` Item `hv` by (Nonempty @List `ha_` Item `hv` Unit `ha` Next `ha_` Item `hv` Unit `ha` Last)
  `ha__` Last `ho_` Item `hv` by (Nonempty @List `ha_` Item `hv` Unit `ha` Next `ha_` Item `hv` Unit `ha` Next `ha_` Item `hv` Unit `ha` Last)
 
+quantifier letter current = current
+ `yokl` Forth `ha` Run `ha` Only `ha` letter
+ `lu__` push `hv` Unit `ho` that `hv` current
+
+-- 1. Use a Scrolling Tree instead of a Scrolling List
+-- 2. Put a proper quantifier name in each variable slot
 positions x = to @(Scrolling List)
  `hv` (x `yo` Glyph `ha` Letter `ha` Lower `ha` I)
  `kyo` unwrap @AR @(Unit `L` Scrolling List `WR` _)
