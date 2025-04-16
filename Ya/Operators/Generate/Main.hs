5a79951e0ceb131cfb960313c6e2179857598fd1
Murat Kasimov <iokasimov.m@gmail.com>
2025-04-16 13:23:37 +0700

    Use list literals, add renaming functions

--- Ya/Operators/Generate/Main.hs
+++ Ya/Operators/Generate/Main.hs
@@ -26,21 +26,26 @@ print = this `he'ho'he` (shaft `hv` by Passed) `ho` this `ho_'yokl` Forth `ha` W
  `lo__'yp` Some `hu_` output `ha` Glyph `ha` Symbol `ha` Punctuate `ha` Comma `hv` Unit
  `lo__'yp` Some `hu_` output `ha` Glyph `ha` Symbol `ha` Punctuate `ha` Space `hv` Unit
 
-arguments = Nonempty @List
- `ha__` Next `ho_` Item `hv` by (Nonempty @List `ha_` Item `hv` Unit `ha` Last)
- `ha__` Next `ho_` Item `hv` by (Nonempty @List `ha_` Item `hv` Unit `ha` Next `ha_` Item `hv` Unit `ha` Last)
- `ha__` Last `ho_` Item `hv` by (Nonempty @List `ha_` Item `hv` Unit `ha` Next `ha_` Item `hv` Unit `ha` Next `ha_` Item `hv` Unit `ha` Last)
+type Arity = Nonempty List Unit
+
+arguments = is @(Nonempty List _) ["#", "##", "###"]
 
 quantifier letter current = current
  `yokl` Forth `ha` Run `ha` Only `ha` letter
  `lu__` push `hv` Unit `ho` that `hv` current
 
+variable = Glyph `ha` Letter `ha` Lower
+ `ha__` None `hu` by I `la` Some `hu` by T
+ `ha__` sub `ho` this @(List `WR` Tree ASCII)
+
+rename = (top `ho` that `lo` variable) `ho'hd` is
+
 -- 1. Use a Scrolling Tree instead of a Scrolling List
 -- 2. Put a proper quantifier name in each variable slot
-positions x = to @(Scrolling List)
- `hv` (x `yo` Glyph `ha` Letter `ha` Lower `ha` I)
+positions x = is @Arity x
+ `yo` Glyph `ha` Letter `ha` Lower `ha` I
+ `yi` to @(Scrolling List)
  `kyo` unwrap @AR @(Unit `L` Scrolling List `WR` _)
- `yi_` to @(Nonempty List)
 
-main = by arguments `yo` positions
- `yokl'yokl`  Forth `ha` Forth `ha` World `ha` print `ha` to @(Scrolling Tree)
+main = arguments `yo` positions `ho` to @(Nonempty List)
+ `yokl'yokl` Forth `ha` Forth `ha` World `ha` print `ha` to @(Scrolling Tree)
