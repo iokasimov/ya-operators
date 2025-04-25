5c8997a1f9f03cc30b0077eb0208b4a5d6e4ba60
Murat Kasimov <iokasimov.m@gmail.com>
2025-04-25 13:00:35 +0700

    Postpone naming varibles to constructing trees

--- Ya/Operators/Generate/Main.hs
+++ Ya/Operators/Generate/Main.hs
@@ -27,12 +27,6 @@ quantified letter current = current
 
 -- I think I should reimplement Scrolling Tree, so we have a Scrolling List as descendants
 
-rename :: Scrolling List Latin `AR` Scrolling List Latin
-rename x = focus `ho` that `hv` x `hv` by (Only `ha` A)
-
-target :: Scrolling List Latin `AR__` Tree Latin
-target x = Construct (Node (by T) (x `yi` rename `yo` intro @Tree `ho'he` is `yi` to @(Nonempty List) `ho` to @List))
-
 print_subtree subtree = Empty @List `hu` enter @World
  `la` Same `hu` (subtree `yokl` Forth `ha` World `ha` print) `ho'yu` Unit
  `li` unwrap subtree
@@ -45,22 +39,23 @@ print = Some `hu_` output `ha` Glyph `ha` Symbol `ha` Punctuate `ha` Space `hv` 
 
 -- TODO: Use a Scrolling Tree instead of a Scrolling List
 -- TODO: Put a proper quantified name in each variable slot
--- positions :: Nonempty List Unit `AR_` Nonempty List `T'I` Scrolling List Latin
-positions x = is @Arity x `yo` I `yi` to @(Scrolling List)
+positions :: Nonempty List Unit `AR___` List `T'I` Scrolling List Unit
+positions x = is @Arity x `yi` to @(Scrolling List)
  `kyo` unwrap @AR @(Unit `L` Scrolling List `WR` _)
  `yi_` to @(Nonempty List) `ho` to @List
  -- `kyo` unwrap @AR @(Unit `L` No List `WR` _)
 
--- operators :: List (Scrolling List Latin)
+operators :: List `T'I` Scrolling List Unit
 operators = to @List `hv` arguments `yok` Cascade `ha` positions
 
 atop (These x y) = Nonempty @List `ha` Item x `ha` Next `ha` Item y `ha` Last
 
 -- Since traversables uses applicative instance inside - can I use Cross label here?
--- combine :: List (List (Scrolling List Latin))
+combine :: List `T'I` Scrolling List Unit `AR___` List `T'I` Nonempty List (Scrolling List Unit)
 combine operators = operators `lu` Cross operators `yp'yo` by `ha` atop
 
-inject x = to @List `ha` to @(Nonempty List) `ha_` focus `ho` that `hv` x `ha_` Only
+inject :: Scrolling List `T'I` Tree Unit `AR___` Tree Latin `AR__` List `T'I` Tree Latin
+inject x = to @List `ha` to @(Nonempty List) `ha_` focus `ho` that `hv` (x `yo'yo` I) `ha_` Only @(Tree Latin)
 
 -- TODO: stateful variable length
 layer x = enter @(State `T'I` Tree Latin)
@@ -71,7 +66,7 @@ layer x = enter @(State `T'I` Tree Latin)
 main = combine operators
  `yokl` Forth `ha` World
  `ha__'yuk` World `ha` output `ha` Caret `ha` Newline `hv` Unit
- `ha__` is @(Nonempty List `T'I` Scrolling List Latin)
+ `ha__` is @(Nonempty List `T'I` Scrolling List Unit)
   `ho_'yokl` Forth `ha` New `ha` layer `ha'yo` intro @Tree
   `ho_'he'he'hv` intro @Tree `hv` by A
   `ho_` print `ha` that @(Tree Latin)
