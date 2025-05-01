10cf747a356d492c4985ea5c8ecc4c8e8c74a8f3
Murat Kasimov <iokasimov.m@gmail.com>
2025-05-01 13:06:38 +0700

    Do not convert to List due new Cross monoidal instance

--- Ya/Operators/Generate/Main.hs
+++ Ya/Operators/Generate/Main.hs
@@ -26,18 +26,18 @@ name symbol variables = variables
 
 data Variable = Parametric | Positioned
 
-type Counter label = label `L` Nonempty List `WR` Unit
+type Counter label = label `L` Nonempty List `T'I` Unit
 
 type Namespace = Counter Parametric `P` Counter Positioned
 
-layer x = enter @(State `WR_` Namespace `P` Tree Name)
+layer x = enter @(State `T'I` Namespace `P` Tree Name)
  `yuk____` New `ha` State `hv___` Event `hv_` get `ha__` Scope `hv` at @(Tree Name)
  `lu___'yp` New `ha` State `hv___` Event `hv_'he'he` name I `ha` that `hv'he` x `ha__` Scope `hv` at @(Counter Positioned)
  `yok____` New `ha` State `ha___` Event `ha_` put `ha` inject `ho__'ha` Scope `hv` at @(Tree Name) `ho_` Scope `hv` sub @Tree
  `yuk____` New `ha` State `hv___` Event `hv_'he'he` name T `ha` this `hv'he` x `ha__` Scope `hv` at @(Counter Parametric)
  `yok____` New `ha` State `ha___` Event `ha_` put @Name `ha'he` is `ho__'ha` Scope `hv` at @(Tree Name) `ho_` Scope `hv` top @Tree
 
-inject = to @List `ha` to @(Nonempty List) `ha` is @(Scrolling List `WR` Tree Name)
+inject = to @List `ha` to @(Nonempty List) `ha` is @(Scrolling List `T'I` Tree Name)
  `ha__` Only `ha` this @(Tree Name) `lo` that @(Shafted List Name) `ho'yo` intro @Tree
 
 initial = wrap [Unit] `lu` wrap [Unit] `lu` intro @Tree [by E]
@@ -50,16 +50,15 @@ positions x = x `yi` to @(Scrolling List) `kyo` Range
 
 parameters = arguments `yok` Plane `ha` to @(Nonempty List) `ha` positions
 
-operators = Nonempty @List
- `ha___` Next `ho` Item (parameters `yo` intro @(Nonempty List) `yi` to @List)
- `ha___` Last `ho` Item (to @List `hv` parameters `lu` Cross `ha` to @List `hv` parameters `yp'yo` atop)
+templates = Nonempty @List
+ `ha__` Next `ho` Item (parameters `yo` intro @(Nonempty List))
+ `ha__` Last `ho` Item (parameters `lu` Cross `hv` parameters `yp'yo` atop)
 
 atop (These x y) = Nonempty @List `ha` Item x `ha` Next `ha` Item y `ha` Last `hv` Unit
 
-main = by operators
+main = by templates
  `yokl'yokl` Forth `ha` Forth `ha` World
  `ha__'yuk` World `ha` output `ha` Caret `ha` Newline `hv` Unit
  `ha__` is @(Nonempty List `T'I` Scrolling List Unit)
-  `ho_'yokl` Forth `ha` New `ha` layer
-  `ho_'he'he'hv` initial
+  `ho_'yokl` Forth `ha` New `ha` layer `ho_'he'he'hv` initial
   `ho_` print `ha` that @(Tree Name) `ha` that
