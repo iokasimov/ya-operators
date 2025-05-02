548cadcd82eedbe82e9baeb71e59c3c14036458a
Murat Kasimov <iokasimov.m@gmail.com>
2025-05-02 18:12:00 +0700

    Include variance (co/contra) into generation functorial targets

--- Ya/Operators/Generate/Main.hs
+++ Ya/Operators/Generate/Main.hs
@@ -18,21 +18,9 @@ type Counter label = label `L` Nonempty List `T'I` Unit
 
 type Namespace = Counter Positioned `P` Counter Parametric
 
-layer x = enter @(State `T'I` Namespace `P` Tree Name)
- `yuk____` New `ha` State `hv___` Event `hv_` get `ha__` Scope `hv` at @(Tree Name)
- `lu___'yp` New `ha` State `hv___` Event `hv_'he'he` name I `ha` that `hv'he` x `ha__` Scope `hv` at @(Counter Positioned)
- `yok____` New `ha` State `ha___` Event `ha_` put `ha` inject `ho__'ha` Scope `hv` at @(Tree Name) `ho_` Scope `hv` sub @Tree
- `yuk____` New `ha` State `hv___` Event `hv_'he'he` name T `ha` this `hv'he` x `ha__` Scope `hv` at @(Counter Parametric)
- `yok____` New `ha` State `ha___` Event `ha_` put @Name `ha'he` is `ho__'ha` Scope `hv` at @(Tree Name) `ho_` Scope `hv` top @Tree
-
-inject = to @List `ha` to @(Nonempty List) `ha` is @(Scrolling List `T'I` Tree Name)
- `ha__` Only `ha` this @(Tree Name) `lo` that @(Shafted List Name) `ho'yo` intro @Tree
-
 templates = Nonempty @List
  `ha__` Next `ho` Item (parameters `yo` intro @(Nonempty List))
  `ha__` Last `ho` Item (parameters `lu` Cross `hv` parameters `yp'yo` Both `ho` to @(Nonempty List))
-
-initial = wrap [Unit] `lu` wrap [Unit] `lu` intro @Tree [by O]
 
 render = Some `hu_` output `ha` Glyph `ha` Symbol `ha` Punctuate `ha` Space `hv` Unit
  `lo__'yp` Some `hu_` Await `ha` output `ha` Glyph `ha` Symbol `ha` Bracket `ha` Opened `hv` Round
@@ -46,16 +34,40 @@ render_subtree subtree = Empty @List `hu` enter @World
 
 type Arity = Nonempty List Unit
 
-arguments = is @(Nonempty List Arity) ["#", "##", "###"]
+arguments = is @(Nonempty List Arity) ["#", "##"] -- , "###"]
 
-positions x = x `yi` to @(Scrolling List) `kyo` Range `ha` is @(Scrolling List _)
+positions x = x
+  `yi` to @(Scrolling List)
+ `kyo` Range `ha` is @(Scrolling List _)
+ `yi_` to @(Nonempty List)
 
-parameters :: Nonempty List (Scrolling List Unit)
-parameters = arguments `yok` Plane `ha` to @(Nonempty List) `ha` positions
+type Variance = Unit `S` Unit
+
+pattern Co e = This e
+pattern Contra e = That e
+
+variances = Both @(P) `ho` to @(Nonempty List)
+ `ha__` (`lu` by Co) `lo` (`lu` by Contra)
+
+parameters = arguments
+ `yok` Plane `ha` positions
+ `yok` Plane `ha` variances
+
+layer (These x v) = enter @(State `T'I` Namespace `P` Tree Name `P` List Variance)
+ `yuk____` New `ha` State `hv___` Event `hv_` get `ha__` Scope `hv` at @(Tree Name)
+ `lu___'yp` New `ha` State `hv___` Event `hv_'he'he` name I `ha` that `hv'he` x `ha__` Scope `hv` at @(Counter Positioned)
+ `yok____` New `ha` State `ha___` Event `ha_` put `ha` inject `ho__'ha` Scope `hv` at @(Tree Name) `ho_` Scope `hv` sub @Tree
+ `yuk____` New `ha` State `hv___` Event `hv_'he'he` name T `ha` this `hv'he` x `ha__` Scope `hv` at @(Counter Parametric)
+ `yok____` New `ha` State `ha___` Event `ha_` put @Name `ha'he` is `ho__'ha` Scope `hv` at @(Tree Name) `ho_` Scope `hv` top @Tree
+ `yuk____` New `ha` State `hv___` Event `ha_` push @List `hv_` v `ha__` Scope `hv` at @(List Variance)
+
+inject = to @List `ha` to @(Nonempty List) `ha` is @(Scrolling List `T'I` Tree Name)
+ `ha__` Only `ha` this @(Tree Name) `lo` that @(Shafted List Name) `ho'yo` intro @Tree
 
 main = by templates
  `yokl'yokl` Forth `ha` Forth `ha` World
  `ha__'yuk` World `ha` output `ha` Caret `ha` Newline `hv` Unit
- `ha__` is @(Nonempty List `T'I` Scrolling List Unit)
-  `ho_'yokl` Forth `ha` New `ha` layer `ho_'he'he'hv` initial
-  `ho_` render `ha` that @(Tree Name) `ha` that
+ `ha__` is @(Nonempty List `T'I` Scrolling List Unit `P` Variance)
+  `ho_'yokl` Forth `ha` New `ha` layer
+  `ho_'he'he'hv` (wrap [Unit] `lu` wrap [Unit] `lu` intro @Tree [by E] `lu` empty @List)
+  `ho_` render `ha` that @(Tree Name) `ha` this `ha` that
